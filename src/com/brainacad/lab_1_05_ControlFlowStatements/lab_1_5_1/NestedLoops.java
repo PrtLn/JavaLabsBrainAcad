51459bcaf06d56d68b5b7a934c3ca2fe03aad966
oleno <pirit.lena@gmail.com>
2016-11-13 21:54:45 +0200

    1.05. Control Flow Statements

---
+++ src/com/brainacad/lab_1_05_ControlFlowStatements/lab_1_5_1/NestedLoops.java
@@ -0,0 +1,64 @@
+package com.brainacad.lab_1_05_ControlFlowStatements.lab_1_5_1;
+
+/**
+ * Created by L.Pikalova on 03/09/2016.
+ * Lab Work 1-5-1
+ Goal:
+    Getting a practical experience in
+        -Writing simple console application, using standard output and nested loop.
+ Task:
+    Write a program to produce on console the following :
+  
+ 1
+ 2  1
+ 3 2  1
+ 4 3 2  1
+ 5 4 3 2  1
+ 6 5 4 3 2  1
+ 7 6 5 4 3 2  1
+ 8 7 6 5 4 3 2  1
+
+ Preparation:   No special preparation
+
+ Execution\ methodology:
+    Remind that  student should apply his or her knowledge of:
+        how to use loops and nested loops
+    Execute following steps:
+        - Student should create a new blank Java Project in IDE.
+        - Student should create his or her own Java class and provide all necessary code in a method main()
+        - Student should use System.out.println() and loops.
+    Ask student to execute the program
+
+ Evaluation:   the program must work correctly;
+ Closure:      Issues discussion, on demand
+ Reporting:    No special reporting
+ */
+
+public class NestedLoops {
+    public static void main(String[] args) {
+
+// correct
+        for (int i = 1; i < 9; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.print(j + " "); // row
+            }
+            System.out.println();  // skip to new row
+        }
+
+/*// j increases
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < i ; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(i);
+        }*/
+
+/*// j is maximum and decreases, i increases
+        for (int i = 1; i < 10; i++) {       // i -> columns
+            for (int j = i; j < 9; j++) {     // j -> rows
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }*/
+    }
+}
---
+++ src/com/brainacad/lab_1_05_ControlFlowStatements/lab_1_5_4/NumberExept.java
@@ -0,0 +1,61 @@
+package com.brainacad.lab_1_05_ControlFlowStatements.lab_1_5_4;
+
+/**
+ * Created by L.Pikalova on 09/04/2016.
+ * Lab Work 1-5-4
+ Goal:
+    Getting a practical experience in
+        Writing simple console application, using nested loop end if-else construction.
+ Task:
+    The program must print the first 10 numbers that is divisible evenly by 3 or 4 , but the for loop iterates from 1 to 300.
+        (so break statement must be used after 10 printed result).
+    The output shall look:
+             3
+             4
+             …
+             20
+
+ Preparation:    No special preparation
+
+ Execution\ methodology:
+    Remind that  student should apply his or her knowledge of:
+        how to use if-else statement
+        how to use loops and nested loops
+    Execute following steps:
+        - Student should create a new blank Java Project in IDE.
+        - Student should create his or her own Java class and provide all necessary code in a method main()
+        - Student should use if-else statment and nested loop.
+    Ask student to execute the program
+
+ Evaluation:  the program must work correctly;
+ Closure:     Issues discussion, on demand
+ Reporting:   No special reporting
+ */
+
+public class NumberExept {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int counter = 0;
+
+        for (int i = 0; i < 300; i++) {
+            if (i == a * 3 || i == a * 4) {
+                System.out.println(i);
+                    a++;
+                    counter++;
+            } else if (counter == 10) {
+                break;
+            }
+        }
+
+        /*
+        for (int i = 1, counter = 0; i < 300 *//* || counter < 10 *//*; i++) {
+            if (counter >= 10) { break; } // comment line in case uncommenting for-condition
+            if (i % 3 == 0 || i % 4 == 0) {
+                System.out.println(i);
+                counter++;
+            }
+        }
+        */
+    }
+}
